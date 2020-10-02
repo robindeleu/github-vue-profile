e7d697862e8795ee82299f2ae3a6aae48c8e6a9f
Robin Deleu <deleu.robin@outlook.com>
2020-10-02 18:45:54 +0200

    styling aangepast

--- src/App.vue
+++ src/App.vue
@@ -39,52 +39,56 @@
 
     <v-main>
       <HelloWorld v-show="false"/>
+<v-card  v-if="profile"
+    class="mx-auto"
+    color="#26c6da"
+    dark
+    max-width="400"
+  >
+    <v-card-title>
+      <span class="title font-weight-light">{{ profile.login }}</span>
+    </v-card-title>
 
-    <v-container v-if="profile">
-      <v-row justify="center">
-      <v-col cols="4">
-        <h2>{{ profile.login }}</h2>
-        <h3> The real name is: {{profile.name}}</h3>
-      </v-col>
-    </v-row>
-    </v-container>
+    <v-card-text class="headline font-weight-bold">
+      <p class="title font-weight-light">Followers: {{profile.followers}}</p>
+      <p class="title font-weight-light">Repository's: {{profile.public_repos}}</p>
+    </v-card-text>
 
-    <v-container v-if="profile">
-      <v-row justify="center">
-      <v-col cols="4">
-        <v-img :src="profile.avatar_url" contain></v-img>
-      </v-col>
-    </v-row>
-    </v-container>
+    <v-card-actions>
+      <v-list-item class="grow">
+        <v-list-item-avatar color="grey darken-3">
+          <v-img
+            class="elevation-6"
+            alt=""
+            :src="profile.avatar_url"
+          ></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>{{profile.name}}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card-actions>
     
-    <v-container v-if="profile">
-      <v-row justify="center">
-      <v-col cols="4">
-        <p>Works in {{profile.company}}</p>
+  </v-card>
+<v-card  v-else
+    class="mx-auto"
+    color="#26c6da"
+    dark
+    max-width="700"
+  >
+   <v-row justify="center">
+      <v-col cols="7">
+        <v-card-title>Please enter a Github username in the texbox below</v-card-title>
+        <v-img  src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"></v-img>
       </v-col>
     </v-row>
-    </v-container>
-
-    <v-container v-if="profile">
-      <v-row justify="center">
-      <v-col cols="4">
-        <p>This is the ammount of followers: {{profile.followers}}</p>
-      </v-col>
-    </v-row>
-    </v-container>
-  
-    <v-container v-if="profile">
-      <v-row justify="center">
-      <v-col cols="4">
-        <p>This is the ammount of repository's: {{profile.public_repos}}</p>
-      </v-col>
-    </v-row>
-    </v-container> 
-
+  </v-card>
+    
+    
     <v-row justify="center">
       <v-col cols="4">
-        <input id="name" v-model="name" type="text" name="name">
-        <p>You're searching for {{name}}</p>
+        <input id="name" v-model="name" type="text" name="name" class="grey" >
       </v-col>
     </v-row>
 
@@ -93,6 +97,7 @@
         <v-btn color=#03A9F4 elevation="2" rounded @click="fetch(name)">Fetch Profile</v-btn>
       </v-col>
     </v-row>
+    
 
     </v-main>
   </v-app>
